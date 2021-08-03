2ded1fc2ab74dc57e7d63dab31bcd125eac8ae36
markianholovchak <markiangolovchak@gmail.com>
2021-08-03 20:22:05 +0200

    Created logo

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__logo">
+        <div className="header__logo--main">
+          Work<span>It</span>
+        </div>
+        <div className="header__logo--sub">Best job search platform</div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
